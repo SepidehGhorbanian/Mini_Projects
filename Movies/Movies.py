7276c529139243dcbe5d9279691ff1678ae28027
Unisepp <106227374+Unisepp@users.noreply.github.com>
2022-08-14 21:41:59 +0430

    Add files via upload

---
+++ Movies/Movies.py
@@ -0,0 +1,29 @@
+
+"""
+
+@author: UNISEPP
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+Movies = pd.read_csv('Movies_Dataset.csv')
+# print(Movies.info())
+
+Movies.rename(columns = {'Gross % US' : 'Gross'} , inplace = True)
+
+Studios = [ 'Buena Vista Studios' , 'Sony' , 'Universal' , 'WB' , 'Fox' , 'Paramount Pictures']
+Genres = ['action' , 'comedy' , 'adventure' ,'drama' , 'animation']
+
+# filtering data
+temp1 = Movies[Movies.Studio.isin(Studios)]
+temp2 = temp1[temp1.Genre.isin(Genres)]
+
+sns.set_style('darkgrid')
+vis1 = sns.stripplot( data = temp2, x = 'Genre' , y='Gross' , hue='Studio' )
+vis2 = sns.boxplot(data = temp2, x= 'Genre' , y = 'Gross' , color = 'lightgray')
+vis1.set(title = 'Domestic Gross % by Genre')
+vis1.set(ylabel = 'Gross % US')
+vis1.legend(bbox_to_anchor = (1,1), loc = 2 )
+
